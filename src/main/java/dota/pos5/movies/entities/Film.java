d2d00d7d8e90eede65f86a50d9bf3a3288032b46
Azamat Chambulov <chambulov2019@gmail.com>
2024-03-25 00:56:36 +0600

    Entities creating

---
+++ src/main/java/dota/pos5/movies/entities/Film.java
@@ -0,0 +1,43 @@
+package dota.pos5.movies.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import dota.pos5.movies.DateForFilms;
+
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Film {
+
+    @PositiveOrZero
+    private Integer id;
+
+    @NotBlank(message = "Не правильное название фильма")
+    private String name;
+
+    @NotNull(message = "Отсутствует описание фильма")
+    @Size(max = 200, message = "слишком длинное описание, больше 200 символов")
+    private String description;
+
+    @NotNull(message = "Отсутствует дата релиза фильма фильма")
+    @DateForFilms
+    private LocalDate releaseDate;
+
+    @Min(value = 1, message = "Неправильная продолжительность фильма")
+    private int duration;
+
+    private Mpa mpa = new Mpa();
+
+    private List<Genre> genres = new ArrayList<>();
+
+    private List<Integer> likes = new ArrayList<>();
+}
---
+++ src/main/java/dota/pos5/movies/entities/User.java
@@ -0,0 +1,51 @@
+package dota.pos5.movies.entities;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+public class User {
+
+    @PositiveOrZero
+    private Integer id;
+
+    private String name;
+
+    @NotBlank(message = "Отсутствует email")
+    @Email(message = "Некорректный email")
+    private String email;
+
+    @NotBlank(message = "логин пуст")
+    private String login;
+
+    @NotNull(message = "Не указана дата рождения")
+    @PastOrPresent(message = "Некорректная дата рождения")
+    private LocalDate birthday;
+
+    private Set<Integer> friends = new HashSet<>();
+
+    public User(Integer id, String name, String email, String login, LocalDate birthday) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.login = login;
+        this.birthday = birthday;
+    }
+
+    public User(Integer id, String name, String email, String login, LocalDate birthday, Set<Integer> friends) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.login = login;
+        this.birthday = birthday;
+        this.friends = friends;
+    }
+}
