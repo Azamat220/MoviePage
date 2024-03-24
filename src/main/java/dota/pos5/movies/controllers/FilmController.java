2f0fe489ee54a562f12270dba3216e3a2a8aaf2e
Azamat Chambulov <chambulov2019@gmail.com>
2024-03-25 02:08:13 +0600

    adding controllers and controllers logic

---
+++ src/main/java/dota/pos5/movies/controllers/FilmController.java
@@ -0,0 +1,66 @@
+package dota.pos5.movies.controllers;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import dota.pos5.movies.entities.Film;
+import dota.pos5.movies.services.FilmService;
+
+import javax.validation.Valid;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/films")
+public class FilmController {
+
+    private final FilmService filmService;
+
+    @PostMapping
+    public ResponseEntity<?> createFilm(@RequestBody @Valid Film film) {
+        filmService.create(film);
+        return new ResponseEntity<>(film, HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public  ResponseEntity<?> updateFilm(@Valid @RequestBody Film film) {
+        filmService.update(film);
+        return new ResponseEntity<>(film, HttpStatus.OK);
+    }
+
+    @GetMapping()
+    public ResponseEntity<?> getFilms() {
+        return new ResponseEntity<>(filmService.getFilms(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{filmId}")
+    public ResponseEntity<?> getFilm(@PathVariable Integer filmId) {
+        return new ResponseEntity<>(filmService.getFilm(filmId), HttpStatus.OK);
+    }
+
+    @PutMapping("/{filmId}/like/{userId}")
+    public ResponseEntity<?> addLike(@PathVariable Integer filmId, @PathVariable Integer userId) {
+        filmService.addLike(filmId, userId);
+        return new ResponseEntity<>("Лайк добавлен", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{filmId}/like/{userId}")
+    public ResponseEntity<?> deleteLike(@PathVariable Integer filmId, @PathVariable Integer userId) {
+        filmService.deleteLike(filmId, userId);
+        return new ResponseEntity<>("Лайк удалён", HttpStatus.OK);
+    }
+
+    @GetMapping("/popular")
+    public ResponseEntity<?> getTopFilms(@RequestParam(defaultValue = "10") int count) {
+        return new ResponseEntity<>(filmService.getMostPopularFilms(count), HttpStatus.OK);
+    }
+
+    @ExceptionHandler()
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleNotFoundException(final IllegalStateException e) {
+        return Map.of("Error", e.getMessage());
+    }
+}
---
+++ src/main/java/dota/pos5/movies/controllers/UserController.java
@@ -0,0 +1,72 @@
+package ru.yandex.practicum.javafilmorate.controllers;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import dota.pos5.movies.entities.User;
+import dota.pos5.movies.services.UserService;
+
+import javax.validation.Valid;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(value = "/users", produces = "application/json")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<?> create(@Valid @RequestBody User user) {
+        userService.create(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> updateUser(@Valid @RequestBody User user) {
+        userService.update(user);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getUsers() {
+        return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getUser(@PathVariable Integer userId) {
+        return new ResponseEntity<>(userService.getUser(userId), HttpStatus.OK);
+    }
+
+    @PutMapping("/{userId}/friends/{friendId}")
+    public ResponseEntity<?> addFriend(@PathVariable Integer userId, @PathVariable Integer friendId) {
+        userService.addFriend(userId, friendId);
+        return new ResponseEntity<>("Пользователь успешно добавлен в друзья", HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{userId}/friends/{friendId}")
+    public ResponseEntity<?> deleteFriend(@PathVariable Integer userId, @PathVariable Integer friendId) {
+        userService.removeFriend(userId, friendId);
+        return new ResponseEntity<>("Пользователь успешно удалён из списка друзей", HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}/friends")
+    public ResponseEntity<?> getUsersFriends(@PathVariable Integer userId) {
+        return new ResponseEntity<>(userService.getUserFriends(userId), HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}/friends/common/{otherId}")
+    public ResponseEntity<?> getCommonFriends(@PathVariable Integer userId, @PathVariable Integer otherId) {
+        return new ResponseEntity<>(userService.getCommonFriends(userId, otherId), HttpStatus.OK);
+    }
+
+    @ExceptionHandler()
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleNotFoundException(final IllegalStateException e) {
+        return Map.of("Error", e.getMessage());
+    }
+
+}
